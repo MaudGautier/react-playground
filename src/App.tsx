e6cfaa7588145733a30acd80b3190672cad7bdeb
Maud Gautier <maud.j.gautier@gmail.com>
2024-01-01 17:57:17 -0700

    chore: nest sub-experiments into correct experiment

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,33 @@ import { OrderOfHooks } from "./experiments/OrderOfHooks";
 import { OrderOfHooksWithHookSelection } from "./experiments/OrderOfHooksWithHookSelection";
 import { Box } from "./components/Box";
 import { OrderOfHooksWithHookSelectionAndInputtedValue } from "./experiments/OderOfHooksWithHookSelectionAndInputtedValue";
+import { DefineStateInitValueInParentComponent } from "./experiments/DefineStateInitValueInParentComponent";
 
 const App = () => (
   <>
     <h1>React playground</h1>
-    <Box title="Case 1: why the 'order of hooks' rule matters 🤯">
-      <OrderOfHooks />
+    <Box title="Experiment 1: Understand the internals of hooks (array in the global state => order matters)">
+      <Box
+        title="Case 1: Why the 'order of hooks' rule matters 🤯"
+        level="case"
+      >
+        <OrderOfHooks />
+      </Box>
+      <Box
+        title="Case 2: Why using a custom hook to select different hooks at re-renders *may* be a problem (⚠️ Not realistic)"
+        level="case"
+      >
+        <OrderOfHooksWithHookSelection />
+      </Box>
+      <Box
+        title="Case 3: Why using a custom hook to select different hooks at re-renders *should not* be a problem (✅ Realistic)"
+        level="case"
+      >
+        <OrderOfHooksWithHookSelectionAndInputtedValue />
+      </Box>
     </Box>
-    <Box title="Case 2: why using a custom hook to select different hooks at re-renders *may* be a problem (⚠️ Not realistic)">
-      <OrderOfHooksWithHookSelection />
-    </Box>
-    <Box title="Case 3: why using a custom hook to select different hooks at re-renders *should not* be a problem (✅ Realistic)">
-      <OrderOfHooksWithHookSelectionAndInputtedValue />
+    <Box title="Experiment 2: Understand why it is pointless to update an initial value">
+      <DefineStateInitValueInParentComponent />
     </Box>
   </>
 );
--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,12 +1,28 @@
 import React, { ReactNode } from "react";
 
+type Level = "experiment" | "case";
+
 type Props = {
   children: ReactNode;
   title?: string;
+  level?: Level;
 };
 
-export const Box = ({ children, title }: Props) => (
-  <div style={{ border: "1px solid black", padding: "10px", margin: "5px" }}>
+const STYLES: Record<Level, object> = {
+  experiment: {
+    border: "2px solid black",
+    padding: "10px",
+    margin: "5px",
+  },
+  case: {
+    border: "1px dashed black",
+    padding: "10px",
+    margin: "5px",
+  },
+};
+
+export const Box = ({ children, title, level = "experiment" }: Props) => (
+  <div style={STYLES[level]}>
     <h2>{title}</h2>
     {children}
   </div>
